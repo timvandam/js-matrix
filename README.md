84ba1f584888b49c5f5976a9daa9aa4850c79ca3
Tim van Dam <timvandamcs@gmail.com>
2018-02-12 18:27:40 +0100

    Initial file upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# node-matrix-lib
+This JavaScript module allows you to easily use matrices in JavaScript.
+
+## Constructor
+Instancing a Matrix is really easy. All you need to provide is a list/object of arguments as shown below.
+```
+const Matrix = require('index.js');
+
+const MyNewMatrix = new Matrix({
+	rows: 3,
+	columns: 3,
+	matrix: [1, 2, 3,
+			 4, 5, 6,
+			 7, 8, 9]
+});
+
+const MyOtherMatrix = new Matrix(3, 3, [9, 8, 7,
+									  	6, 5, 4,
+									  	3, 2, 1]);
+```
+
+## Methods
+
+Each method returns a new `Matrix`, they do not change their parant Matrix class.
+
+### multiply(multiplier) or .mult(multiplier)
+* `multiplier` - A `Matrix` instance or scalar
+
+### add(addition)
+* `addition` - A `Matrix` instance
+
+### subtract(substration) or .sub(substraction)
+* `substraction` - A `Matrix` instance
+
+## Info
+This module was created just for fun, I wanted to make a module so I decided to make this. If you have any suggestions feel free to send me an email. (timvandamcs@gmail.com)
---
+++ index.js
@@ -0,0 +1,70 @@
+function Matrix(args) {
+  if (!(args instanceof Object) || Array.isArray(args))
+    const params = {
+      rows: arguments[0],
+      columns: arguments[1],
+      matrix: arguments[2],
+    };
+  else const params = args;
+
+  if (!Object.keys(params).filter(key => ['rows', 'columns', 'matrix'].indexOf(key) + 1).length ||
+    params.columns * params.rows !== params.matrix.length ||
+    Object.values(params.matrix).filter(value => Number.isNaN(Number(value))).length)
+    throw Error('Invalid constructor parameters.');
+
+  this.rows = params.rows;
+  this.columns = params.columns;
+  this.matrix = params.matrix;
+
+  this.multiply = multiplier => {
+    if (Number.isNaN(Number(multiplier)) && !(multiplier instanceof Matrix))
+      throw Error('Invalid matrix/scalar provided.');
+
+    if (!Number.isNaN(Number(multiplier)))
+      return new Matrix({
+        rows: this.rows,
+        columns: this.columns,
+        matrix: this.matrix.map(value => value * multiplier),
+      });
+
+    const rows = Array(this.rows).fill(0).map((value, index) => this.matrix.slice(index * this.matrix.length / this.rows, index * this.matrix.length / this.rows + this.matrix.length / this.rows));
+    const columns = Array(multiplier.columns).fill(0).map((value, index) => multiplier.matrix.filter((multiplierValue, multiplierIndex) => multiplierIndex % multiplierValue.columns === index));
+
+    return new Matrix({
+      rows: this.rows,
+      columns: multiplier.columns,
+      matrix: Array(this.rows * multiplier.columns).fill(0).map((value, index) => {
+        const row = Math.floor(index / multiplier.columns);
+        const column = index % this.rows;
+        return rows[row].map((value, index) => value * columns[column][index]).reduce((x, y) => x + y);
+      }),
+    });
+  };
+
+  this.add = addition => {
+    if (!(addition instanceof Matrix) || this.columns !== addition.columns || this.rows !== addition.rows || this.matrix.length !== addition.matrix.length) 
+      throw Error('Invalid matrix provided.');
+
+    return new Matrix({
+      rows: this.rows,
+      columns: this.columns,
+      matrix: this.matrix.map((value, index) => value + addition.matrix[index]),
+    });
+  };
+
+  this.subtract = substraction => {
+    if (!(substraction instanceof Matrix) || this.columns !== substraction.columns || this.rows !== substraction.rows || this.matrix.length !== substraction.matrix.length)
+      throw Error('Invalid matrix provided.');
+
+    return new Matrix({
+      rows: this.rows,
+      columns: this.columns,
+      matrix: this.matrix.map((value, index) => value - substraction.matrix[index]),
+    });
+  };
+
+  this.mult = this.multiply;
+  this.sub = this.subtract;
+}
+
+module.exports = Matrix;
